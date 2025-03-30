eb0cf755c529a8c376f0dddb11980c73ff0e26d7
travis@travestyofcode.com <travis@travestyofcode.com>
2025-03-30 18:56:49 -0400

    Adds extension to project from entity to model.

--- MPN.Data/Models/VendorPartNumber.cs
+++ MPN.Data/Models/VendorPartNumber.cs
@@ -26,3 +26,20 @@ public class VendorPartNumber
     [Column(TypeName = "decimal(15,5)")]
     public decimal? VendorCost { get; set; }
 }
+
+internal static class VendorPartNumberExtensions
+{
+    public static IQueryable<VendorPartNumber> ProjectToModel(this IQueryable<Entities.VendorPartNumber> source) =>
+        source.Select(p => new VendorPartNumber()
+        {
+            Id = p.Id,
+            ItemId = p.ItemId,
+            ItemFullName = p.Item!.FullName!, // TODO: Change Item.FullName is not null as we don't allow it on create or update?
+            ItemPartNumber = p.Item.PartNumber,
+            VendorId = p.VendorId,
+            VendorName = p.Vendor!.Name,
+            PartNumber = p.PartNumber,
+            VendorDescription = p.VendorDescription,
+            VendorCost = p.VendorCost
+        });
+}
