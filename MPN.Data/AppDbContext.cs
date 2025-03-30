3bb663f4739edf792788ba26f2bb9cc0bdafd95a
travis@travestyofcode.com <travis@travestyofcode.com>
2025-03-30 09:48:07 -0400

    Adds DBContext and AppUser and configures design time context.

---
+++ MPN.Data/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace MPN.Data;
+
+internal class AppDbContext : IdentityDbContext<AppUser>
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
+
+internal class DesignTimeAppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var envName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: true)
+            .AddJsonFile($"appsettings.{envName}.json", optional: true)
+            .AddEnvironmentVariables()
+            .AddCommandLine(args)
+            .Build();
+
+        var connString = config.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Unable to get connection string.");
+
+        var optionBuilder = new DbContextOptionsBuilder<AppDbContext>()
+            .UseSqlServer(connString);
+
+        return new AppDbContext(optionBuilder.Options);
+    }
+}
