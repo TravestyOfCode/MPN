40c0390e0970930c8b0a01ab30f6719d8dd88f0a
travis@travestyofcode.com <travis@travestyofcode.com>
2025-03-30 18:58:43 -0400

    Adds search request by part number.

---
+++ MPN.Data/Services/VendorPartNumber/SearchVendorPartNumbersByPartNumber.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MPN.Data;
+
+public class SearchVendorPartNumbersByPartNumber : IRequest<Result<List<VendorPartNumber>>>
+{
+    public required string PartNumber { get; set; }
+}
+
+internal class SearchVendorPartNumbersByPartNumberHandler(AppDbContext dbContext, ILogger<SearchVendorPartNumbersByPartNumberHandler> logger) : IRequestHandler<SearchVendorPartNumbersByPartNumber, Result<List<VendorPartNumber>>>
+{
+    public async Task<Result<List<VendorPartNumber>>> Handle(SearchVendorPartNumbersByPartNumber request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return await dbContext.VendorPartNumbers
+                .Include(p => p.Item)
+                .Include(p => p.Vendor)
+                .Where(p => p.PartNumber.StartsWith(request.PartNumber))
+                .ProjectToModel()
+                .ToListAsync(cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Unexpected error handling request: {request}", request);
+
+            return Error.ServerError();
+        }
+    }
+}
